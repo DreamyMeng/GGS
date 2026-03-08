7de31be5099c7c1d96f3dff769d42add5ac7a923
孟无岐 <dragon01032@163.com>
2026-03-09 02:16:32 +0800

    unitask

--- Assets/DoMoveTest.cs
+++ Assets/DoMoveTest.cs
@@ -1,14 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
 using DG.Tweening;
 using UnityEngine;
 
 public class DoMoveTest : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+   async void Start()
     {
-        transform.DOMoveX(transform.position.x + 2, 2f);
+// sequential
+        await transform.DOMoveX(2, 1);
+        await transform.DOMoveZ(5, 2);
+
+// parallel with cancellation
+        var ct = this.GetCancellationTokenOnDestroy();
+
+        await UniTask.WhenAll(
+            transform.DOMoveX(10, 3).WithCancellation(ct),
+            transform.DOScale(10, 3).WithCancellation(ct));
     }
 
     // Update is called once per frame
